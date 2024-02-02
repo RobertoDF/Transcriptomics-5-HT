0c9e0b8346455640eab34ee4dfe63d5be5784a07
RobertoDF <roberto.defilippo@outlook.com>
2024-02-02 19:32:15 +0100

    update

--- Manuscript/abstract.py
+++ Manuscript/abstract.py
@@ -1,8 +1,18 @@
-from Utils.Settings import output_folder_calculations
 
 
 
-abstract = "" \
-
-
-#%%
+abstract = {"This study presents a comprehensive transcriptomic analysis of serotonin (5-HT) "
+            "receptor expression across approximately 4 million brain cells, utilizing single-cell "
+            "RNA sequencing data from the Allen Institute. We report on the differential expression"
+            " patterns of all 14 known Htr subtypes, revealing a wide diversity in their prevalence and "
+            "distribution throughout the brain. Notably, we found that 65.84% of cells transcribe RNA of at least one Htr subtype, "
+            "with significant variability in expression levels and receptor distribution across different brain regions and"
+            " cell types. Pattern of Htrs transcription are sufficient to decode with a random forest classifier "
+            "the identity of some classes and neurotranmitter-releasing cells with"
+            "surprising success. Htrs are transcribed only rarely alone within a single cell. Using a MERFISH dataset we analysed "
+            "the spatial distribution of each Htr confirming previous reports and uncovering novel patterns of possible expression. "
+            "Our findings underscore the complexity of the 5-HT system "
+            "even at the cellular level and"
+            " provide new insights into the receptor-mediated mechanisms that underpin diverse neural functions and behaviors. To aid the exploration of the dataset "
+            "at different level of granularity"
+            "we provide a bespoke online visualizer that enables the investigation of other neuromodulatory systems as well."}
--- Manuscript/methods.py
+++ Manuscript/methods.py
@@ -1,4 +1,5 @@
 
 
 
-methods = {"aa":"",   "Htr1a": ""}
+methods = {"Online visualizer": "The online visualizer was built in python using Matplotlib, Holoviews and Panel. It is deployed and accesible online on the Hugging Face portal "
+                                "https://huggingface.co/organizations/RDFm982367h7ndq/share/sJrEwlxfHoaNuXuQcqXjuZhbqkGbuyqxxa"}
