b821cb2b573df76a3255ddfc57c55effd54785c0
RobertoDF <roberto.defilippo@outlook.com>
2024-06-13 12:46:03 +0200

    update legend Figure 1. Add neurotransmitters acronyms.

--- Figures/legends.py
+++ Figures/legends.py
@@ -13,7 +13,7 @@ legends =  {"Figure 1. Overview of Htrs translation.":
                 f"Inset shows the prevalence of each Htr using  a threshold (log(CPM)>{threshold_expression}). "
                 "(B) UMAP representation color-coded by neighborhood metadata (left), Htr1 (middle) and Htr2 (right) transcription. "
                 "(C) Htr expression prevalence in cells grouped by neurotransmitter release (top). Confusion matrix of the multi-label random forest classifier showing "
-                "true label on y axis and predicted label on x axis (middle). Matrix of absolute SHAP values for each group and receptor (bottom). "
+                "true label on y axis and predicted label on x axis (middle). Matrix of absolute SHAP values for each group and receptor (bottom). Glut = Glutamate, GABA = Gamma-Aminobutyric Acid, Glut-GABA = Glutamate and Gamma-Aminobutyric Acid, Dopa = Dopamine, None = No specific neurotransmitter, GABA-Glyc = Gamma-Aminobutyric Acid and Glycine, Chol = Acetylcholine (Cholinergic), Hist = Histamine, Sero = Serotonin, Nora = Norepinephrine. "
                 "(D) Htr expression prevalence in cells grouped byclass. "
                 "(E) Htrs expression correlation matrix. "
                 "(F) Htrs colocalization matrix. Each dot represents the percentage of colocalization of gene on x axis in cells expressing gene on y axis. "
