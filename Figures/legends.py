8a57165d03e7ae73f6bc9753d926f3ca5851b656
RobertoDF <roberto.defilippo@outlook.com>
2024-06-19 15:10:50 +0200

    update

--- Figures/legends.py
+++ Figures/legends.py
@@ -9,12 +9,12 @@ selected_genes_for_main_fig = ['Htr1a', 'Htr1b', 'Htr1f', 'Htr2a', 'Htr2c',
 
 
 legends =  {"Figure 1. Overview of Htrs translation.":
-                "(A) Barplot showing absolute number of cells transcribing each Htrs, amount of expression is represented in greyscale, no threshold is applied. "
+                "(A) Bar plot showing absolute number of cells transcribing each Htrs, amount of expression is represented in greyscale, no threshold is applied. "
                 f"Inset shows the prevalence of each Htr using  a threshold (log(CPM)>{threshold_expression}). "
                 "(B) UMAP representation color-coded by neighborhood metadata (left), Htr1 (middle) and Htr2 (right) transcription. "
                 "(C) Htr expression prevalence in cells grouped by neurotransmitter release (top). Confusion matrix of the multi-label random forest classifier showing "
                 "true label on y axis and predicted label on x axis (middle). Matrix of absolute SHAP values for each group and receptor (bottom). Glut = Glutamate, GABA = Gamma-Aminobutyric Acid, Glut-GABA = Glutamate and Gamma-Aminobutyric Acid, Dopa = Dopamine, None = No specific neurotransmitter, GABA-Glyc = Gamma-Aminobutyric Acid and Glycine, Chol = Acetylcholine (Cholinergic), Hist = Histamine, Sero = Serotonin, Nora = Norepinephrine. "
-                "(D) Htr expression prevalence in cells grouped byclass. "
+                "(D) Htr expression prevalence in cells grouped by class. "
                 "(E) Htrs expression correlation matrix. "
                 "(F) Htrs colocalization matrix. Each dot represents the percentage of colocalization of gene on x axis in cells transcribing gene on y axis. "
                 "(G) Top: Percentage of cells transcribing the number of Htrs on the x axis. Percentage of cells transcribing the gene on x axis transcribing at least another Htrs gene (middle) or at least other 4 Htrs (bottom). "
@@ -22,13 +22,13 @@ legends =  {"Figure 1. Overview of Htrs translation.":
                 "identified by summing the amount of RNA belonging to recpeptor of the same family in each cell. Each number represents the "
                 "number of cells in thousands. "}
 
-legends.update({f"Figure {n+2}. {gene} transcription": f"(A) On the left, dotplot representing {gene} prevalence across neighborhoods with squared Pearson correlation coefficient (R²) between RNA-seq "
-                                                       f"and MERFISH dataset. On the right, violinplots representing the amount of {gene} RNA detected using "
+legends.update({f"Figure {n+2}. {gene} transcription": f"(A) On the left, dot plot representing {gene} prevalence across neighborhoods with squared Pearson correlation coefficient (R²) between RNA-seq "
+                                                       f"and MERFISH dataset. On the right, violin plots representing the amount of {gene} RNA detected using "
                                               f"RNA-seq (top) and MERFISH (bottom). "
                                               f"(B) Amount of colocalization with each Htrs by cells expressing {gene} RNA in the scRNAseq dataset (left). Number of Htrs RNA detected in cells "
                                               f"expressing {gene} RNA in the scRNAseq dataset  (right). "
                                               f"(C) Prevalence of {gene} RNA across all classes of cells in RNA-seq and MERFISH dataset. Inset represents the linear regression between the two datasets. "
-                                              f"On te right, absolute number of cells expressing {gene} RNA in the scRNAseq by class, ranked in descending order (top ten). "
+                                              f"On the right, absolute number of cells expressing {gene} RNA in the scRNAseq by class, ranked in descending order (top ten). "
                                               f"(D) Ranked prevalence of {gene} RNA across divisions (left) and structures of enriched clusters found in the scRNAseq dataset in the MERFISH dataset(right). "
                                                        f"Inset represents the proportion of cells expressing {gene} RNA that belongs to enriched clusters. "
                                               f"(E) Top: Prevalence of cells from enriched clusters across the antero-posterior axis, "
--- Figures/legends_supplementary.py
+++ Figures/legends_supplementary.py
@@ -3,10 +3,10 @@
 
 legends_supplementary =  {
     "Figure S1. Htrs transcription and hierarchical clustering visualization.":
-                "(A) Top: Linear regression model fit between prevalence (y axis) and amount of intracellular expression (x axis) for each Htrs. Bottom: violinplots "
+                "(A) Top: Linear regression model fit between prevalence (y axis) and amount of intracellular expression (x axis) for each Htrs. Bottom: violin plots "
                 "showing distribution of intracellular amount of expression for each Htrs. "
                 "(B) Heatmaps showing relationship between neurotransmitter, class and neighborhoods. "
-                "(C) Barplots representing the size of each group when cells are categorized by neurotransmitter (top), neighborhood (middle) and class (botttom). ",
+                "(C) Bar plots representing the size of each group when cells are categorized by neurotransmitter (top), neighborhood (middle) and class (botttom). ",
                 
     "Figure S2. UMAP distribution of each Htr": "UMAP-associated neurotransmitter, class and dissection region metadata (first row) and UMAP distribution of each Htr.",
 "Figure S3. Htrs features across neighborhoods and classes": "(A) Htrs prevalence across neighborhoods (left). Confusion matrix related to the decoding of neighboroods "
@@ -20,7 +20,7 @@ legends_supplementary =  {
                                               f"(B) Amount of co-localization with each Htrs by cells expressing Htr1d RNA (left). Number of Htrs RNA detected in cells "
                                               f"expressing Htr1d RNA (right). "
                                               f"(C) Prevalence of Htr1d RNA across all classes of cells in RNA-seq and MERFISH dataset. Inset represents the linear regression between the two datasets. "
-                                              f"On te right, absolute number of cells expressing Htr1d RNA by class ranked in descending order (top ten). "
+                                              f"On the right, absolute number of cells expressing Htr1d RNA by class ranked in descending order (top ten). "
                                               f"(D) Prevalence of Htr1d RNA across divisions (left) and structures (right). Inset represents the proportion of cells expressing Htr1d RNA that belongs to enriched clusters. "
                                                 f"(E) Top: Prevalence of cells from enriched clusters across the antero-posterior axis, "
                                                 f"identified in the scRNA-seq dataset and cross-referenced in the MERFISH dataset. "
@@ -32,7 +32,7 @@ legends_supplementary =  {
                                               f"(B) Amount of co-localization with each Htrs by cells expressing Htr5a RNA (left). Number of Htrs RNA detected in cells "
                                               f"expressing Htr5a RNA (right). "
                                               f"(C) Prevalence of Htr5a RNA across all classes of cells in RNA-seq and MERFISH dataset. Inset represents the linear regression between the two datasets. "
-                                              f"On te right, absolute number of cells expressing Htr5a RNA by class ranked in descending order (top ten). "
+                                              f"On the right, absolute number of cells expressing Htr5a RNA by class ranked in descending order (top ten). "
                                               f"(D) Prevalence of Htr5a RNA across divisions (left) and structures (right). Inset represents the proportion of cells expressing Htr5a RNA that belongs to enriched clusters. "
                                               f"(E) Prevalence of cells from enriched clusters across the antero-posterior axis, "
                                                 f"identified in the scRNA-seq dataset and cross-referenced in the MERFISH dataset. ",
@@ -42,7 +42,7 @@ legends_supplementary =  {
                                               f"(B) Amount of co-localization with each Htrs by cells expressing Htr5b RNA (left). Number of Htrs RNA detected in cells "
                                               f"expressing Htr5b RNA (right). "
                                               f"(C) Prevalence of Htr5b RNA across all classes of cells in RNA-seq and MERFISH dataset. Inset represents the linear regression between the two datasets. "
-                                              f"On te right, absolute number of cells expressing Htr5b RNA by class ranked in descending order (top ten). "
+                                              f"On the right, absolute number of cells expressing Htr5b RNA by class ranked in descending order (top ten). "
                                               f"(D) Prevalence of Htr5b RNA across divisions (left) and structures (right). Inset represents the proportion of cells expressing Htr5b RNA that belongs to enriched clusters. "
                                               f"(E) Prevalence of cells from enriched clusters across the antero-posterior axis, "
                                                 f"identified in the scRNA-seq dataset and cross-referenced in the MERFISH dataset. ",
@@ -52,21 +52,21 @@ legends_supplementary =  {
                                               f"(B) Amount of co-localization with each Htrs by cells expressing Htr2b RNA (left). Number of Htrs RNA detected in cells "
                                               f"expressing Htr2b RNA (right). "
                                               f"(C) Prevalence of Htr2b RNA across all classes of cells in RNA-seq and MERFISH dataset. Inset represents the linear regression between the two datasets. "
-                                              f"On te right, absolute number of cells expressing Htr2b RNA by class ranked in descending order (top ten). ",
+                                              f"On the right, absolute number of cells expressing Htr2b RNA by class ranked in descending order (top ten). ",
     
 "Figure S9. Htr6 transcription": f"(A) On the left, Htr6 prevalence across neighborhoods with squared Pearson correlation coefficient (R²) between RNA-seq and MERFISH dataset. On the right, amount of Htr6 RNA detected using "
                                               f"RNA-seq (top) and MERFISH (bottom). "
                                               f"(B) Amount of co-localization with each Htrs by cells expressing Htr6 RNA (left). Number of Htrs RNA detected in cells "
                                               f"expressing Htr6 RNA (right). "
                                               f"(C) Prevalence of Htr6 RNA across all classes of cells in RNA-seq and MERFISH dataset. Inset represents the linear regression between the two datasets. "
-                                              f"On te right, absolute number of cells expressing Htr6 RNA by class ranked in descending order (top ten). ", 
+                                              f"On the right, absolute number of cells expressing Htr6 RNA by class ranked in descending order (top ten). ",
     
 "Figure S10. Htr3a transcription": f"(A) On the left, Htr3a prevalence across neighborhoods with squared Pearson correlation coefficient (R²) between RNA-seq and MERFISH dataset. On the right, amount of Htr3a RNA detected using "
                                               f"RNA-seq (top) and MERFISH (bottom). "
                                               f"(B) Amount of co-localization with each Htrs by cells expressing Htr3a RNA (left). Number of Htrs RNA detected in cells "
                                               f"expressing Htr3a RNA (right). "
                                               f"(C) Prevalence of Htr3a RNA across all classes of cells in RNA-seq and MERFISH dataset. Inset represents the linear regression between the two datasets. "
-                                              f"On te right, absolute number of cells expressing Htr3a RNA by class ranked in descending order (top ten). "
+                                              f"On the right, absolute number of cells expressing Htr3a RNA by class ranked in descending order (top ten). "
                                               f"(D) Prevalence of Htr3a RNA across divisions (left) and structures (right). Inset represents the proportion of cells expressing Htr3a RNA that belongs to enriched clusters. "
                                               f"(E) Top: Prevalence of cells from enriched clusters across the antero-posterior axis, "
                                                 f"identified in the scRNA-seq dataset and cross-referenced in the MERFISH dataset. "
@@ -78,10 +78,10 @@ legends_supplementary =  {
                                               f"(B) Amount of co-localization with each Htrs by cells expressing Htr3b RNA (left). Number of Htrs RNA detected in cells "
                                               f"expressing Htr3b RNA (right). "
                                               f"(C) Prevalence of Htr3b RNA across all classes of cells in RNA-seq and MERFISH dataset. Inset represents the linear regression between the two datasets. "
-                                              f"On te right, absolute number of cells expressing Htr3b RNA by class ranked in descending order (top ten). ",
+                                              f"On the right, absolute number of cells expressing Htr3b RNA by class ranked in descending order (top ten). ",
 "Table 1. Neighborhoods descriptions": f"Description of cells belonging to each neighborhood group. ",
 "Table 2. Region acronyms": f"Allen Mouse Brain Common Coordinate Framework (CCF) acronyms. ",
-"Table 3. Htrs defining features": f"Concise table listing the defining feature of each Htr trascription pattern. ",
+"Table 3. Htrs defining features": f"Table listing the defining feature of each Htr trascription pattern. ",
     
     
 }
